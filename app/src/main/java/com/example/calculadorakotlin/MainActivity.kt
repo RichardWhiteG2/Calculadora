9915adb3fd687e7d8e8b8888b8bbd137a5f1f6e2
Brayan Ricardo Blanco García <102545085+RichardWhiteG2@users.noreply.github.com>
2022-04-14 12:54:09 -0500

    Funciona operaciones 1 sola vez, falta borrar pantalla y que muestre número.

--- app/src/main/java/com/example/calculadorakotlin/MainActivity.kt
+++ app/src/main/java/com/example/calculadorakotlin/MainActivity.kt
@@ -6,9 +6,14 @@ import android.widget.Button
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    private var num1: Double =0.0
+    private  var num2: Double=0.0
+    private var operacion: Int =0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         val total: TextView= findViewById(R.id.tvTotal)
         val uno: TextView = findViewById(R.id.numero_1)
         val dos: TextView = findViewById(R.id.numero_2)
@@ -28,8 +33,8 @@ class MainActivity : AppCompatActivity() {
         val mas: TextView = findViewById(R.id.signo_mas)
         val menos: TextView = findViewById(R.id.signo_menos)
         val mul: TextView = findViewById(R.id.signo_mul)
-
-
+        total.text="0.0"
+        //Cuando se presiona un numero
         uno.setOnClickListener{(numeropresionado("1"))}
         dos.setOnClickListener{(numeropresionado("2"))}
         tres.setOnClickListener{(numeropresionado("3"))}
@@ -42,11 +47,60 @@ class MainActivity : AppCompatActivity() {
         cero.setOnClickListener{(numeropresionado("0"))}
 
 
+        //Operaciones presionadas
+
+        mas.setOnClickListener {operacionPresionada(Suma)}
+        menos.setOnClickListener {operacionPresionada(resta)}
+        div.setOnClickListener {operacionPresionada(division)}
+        mul.setOnClickListener {operacionPresionada(multiplicacion)}
+
+        //opciones adicionales
+
+        borrar.setOnClickListener {
+            num1=0.0
+            num2=0.0
+            total.text="0.0"
+            operacion= no_operacion
+        }
+
+        igual.setOnClickListener {
+            var resultado = when(operacion){
+                Suma -> num1 +num2
+                resta -> num1 - num2
+                division -> num1 / num2
+                multiplicacion -> num1 *num2
+                else -> 0
+            }
+            total.text=resultado.toString()
+        }
+
     }
+    //Funcion para agregar el número precionado en la vista
     private fun numeropresionado(digito: String){
         val total: TextView= findViewById(R.id.tvTotal)
 
         total.text ="${total.text}$digito"
-    }
 
+        if (operacion== no_operacion){
+            num1 = total.text.toString().toDouble()
+        }else{
+            num2 = total.text.toString().toDouble()
+        }
+    }
+    //Para saber que operacion se va a realizar i guardar el primer valor
+    private fun operacionPresionada(operacion: Int){
+        val total: TextView= findViewById(R.id.tvTotal)
+
+        this.operacion = operacion
+
+        num1 = total.text.toString().toDouble()
+        total.text="0"
+    }
+    companion object{
+        const val Suma=1
+        const val resta=2
+        const val division=3
+        const val multiplicacion=4
+        const val no_operacion=0
+    }
 }
